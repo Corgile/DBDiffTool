704f30a9904b8e16ff3afc9c7b580a741df4f14e
corgile <mashiro-e@qq.com>
2024-11-10 14:53:38 +0800

    [ADD] MySQL statements.

--- include/DBDiffTool/orm/MySQL.hh
+++ include/DBDiffTool/orm/MySQL.hh
@@ -16,7 +16,17 @@ struct MySQL {
     }
     /// 第一级元数据 - schema_table / schema_view
     static std::string const& schema_sql(type const t) {
-        static const std::string table{ R"(SELECT 1;)" };
+        static const std::string table{ R"(
+SELECT
+    LOWER(TABLE_SCHEMA) AS schema_name,
+    LOWER(TABLE_NAME) AS table_name,
+    LOWER(GROUP_CONCAT(COLUMN_NAME ORDER BY LENGTH(COLUMN_NAME),COLUMN_NAME)) AS table_fields,
+    LOWER(GROUP_CONCAT(COLUMN_TYPE ORDER BY LENGTH(COLUMN_NAME),COLUMN_NAME)) AS field_types,
+    LOWER(GROUP_CONCAT(IS_NULLABLE ORDER BY LENGTH(COLUMN_NAME),COLUMN_NAME)) AS field_nulls
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA NOT IN('sys', 'mysql','information_schema')
+GROUP BY TABLE_SCHEMA, TABLE_NAME
+ORDER BY LENGTH(TABLE_SCHEMA),TABLE_SCHEMA,LENGTH(TABLE_NAME),TABLE_NAME)" };
         static const std::string view{ R"(SELECT 1;)" };
         if (t == type::table) return table;
         if (t == type::view) return view;
